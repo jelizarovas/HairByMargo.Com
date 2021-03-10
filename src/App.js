508ac38400e9b0a18ddd74fd75b62c477dc769be
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-03-09 21:50:56 -0800

    baseline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+import { Welcome } from "./components/Welcome";
+import { Services } from "./components/Services";
+import { Gallery } from "./components/Gallery";
+import { About } from "./components/About";
+import { Reviews } from "./components/Reviews";
+import { Team } from "./components/Team";
+import { Contact } from "./components/Contact";
+import { Navbar } from "./components/layout/Navbar";
+import { Footer } from "./components/layout/Footer";
 
 function App() {
-  return <div className="App bg-red-700">Salon Margo welcomes you!</div>;
+  return (
+    <div className="App bg-red-700">
+      <Navbar />
+      <Router basename="/">
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+            <Services />
+            <Gallery />
+            <About />
+            <Reviews />
+            <Team />
+            <Contact />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/gallery">
+            <Gallery />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/reviews">
+            <Reviews />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Team.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Team = () => {
+  return <div>team</div>;
+};
