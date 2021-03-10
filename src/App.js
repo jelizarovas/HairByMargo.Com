d19e090f9a0ad4031aa43e1d27f48545620082c9
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-03-09 21:25:49 -0800

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App bg-red-700">Test</div>;
+  return <div className="App bg-red-700">Salon Margo welcomes you!</div>;
 }
 
 export default App;
