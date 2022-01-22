0ff8338335eb949a4119b24c796537925c9f38d3
Arnas Jelizarovas <jelizarovas@gmail.com>
2022-01-21 22:00:09 -0800

    fixed footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,147 +1,131 @@
 import React from "react";
+import { MdEmail, MdFacebook, MdGroups, MdLockClock, MdMenuBook, MdOutlineMap } from "react-icons/md";
+import { FaInstagram, FaPhone, FaRegClock } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <footer className="page-footer font-small pt-4 bg-dark text-white">
-      <div className="container text-md-left">
-        <div className="row">
-          <div className="col-md-4 mt-md-0 mt-3">
-            <h5 className="text-uppercase pb-2">
-              {/* style="border-bottom: 1px dashed white;" */}
-              <i data-feather="map"></i> Location
-            </h5>
-
-            <div className="row py-1">
-              <div className="col-lg-6">
-                <a
-                  className="text-white"
-                  aria-label="Open Location"
-                  target="_blank"
-                  rel="noopener"
-                  href="https://www.google.com/maps/place/Salon+Margo/@41.6461054,-72.8749937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e7ba83e34186cf:0x6b206121c02dda8d!8m2!3d41.6461054!4d-72.872805"
-                >
-                  <address>
-                    1079 Queen St
-                    <br />
-                    STE #11,
-                    <br />
-                    Southington,
-                    <br />
-                    CT 06489
-                  </address>
-                </a>
-              </div>
-              <div className="col-lg-6">
-                <a
-                  href="https://www.google.com/maps/place/Salon+Margo/@41.6461054,-72.8749937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e7ba83e34186cf:0x6b206121c02dda8d!8m2!3d41.6461054!4d-72.872805"
-                  target="_blank"
-                  rel="noopener noreferrer "
-                >
-                  <picture>
-                    <source
-                      srcSet="
+    <footer
+      className="bg-gray-600 flex  flex-col justify-around py-10  text-white"
+      style={{ backgroundImage: 'url("/images/scissors-bg.jpg")', boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.9)" }}
+    >
+      <div className=" flex container  mx-auto flex-col lg:flex-row justify-around text-white py-3 px-10">
+        <div className="pb-2 lg:w-1/4">
+          <FooterHeader title="location" Icon={MdOutlineMap} />
+          <div className=" py-1 flex justify-between">
+            <a
+              className="text-white "
+              aria-label="Open Location"
+              target="_blank"
+              rel="noopener"
+              href="https://www.google.com/maps/place/Salon+Margo/@41.6461054,-72.8749937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e7ba83e34186cf:0x6b206121c02dda8d!8m2!3d41.6461054!4d-72.872805"
+            >
+              <address className="flex flex-col">
+                <span> 1079 Queen St</span>
+                <span> STE #11,</span>
+                <span>Southington,</span>
+                <span> CT 06489</span>
+              </address>
+            </a>
+            <a
+              href="https://www.google.com/maps/place/Salon+Margo/@41.6461054,-72.8749937,17z/data=!3m1!4b1!4m5!3m4!1s0x89e7ba83e34186cf:0x6b206121c02dda8d!8m2!3d41.6461054!4d-72.872805"
+              target="_blank"
+              rel="noopener noreferrer "
+              className="-mt-6 shadow-lg border-dashed border rounded p-2 bg-gray-600 mr-4"
+            >
+              <picture>
+                <source
+                  srcSet="
                 /images/build/map.webp"
-                      type="image/webp"
-                    />
-                    <source
-                      srcSet="
+                  type="image/webp"
+                />
+                <source
+                  srcSet="
                 /images/map.png"
-                      type="image/png"
-                    />
-                    <img width="150" className="img rounded" src="/images/build/map.webp" alt="location map" />
-                  </picture>
-                </a>
-              </div>
+                  type="image/png"
+                />
+                <img width="150" className="img rounded" src="/images/build/map.webp" alt="location map" />
+              </picture>
+            </a>
+          </div>
+        </div>
+
+        <div className="lg:w-1/4 pb-2 ">
+          <FooterHeader title="social" Icon={MdGroups} />
+          <div className="flex justify-around py-6 text-xl ">
+            <div className="  ">
+              <a
+                href="https://www.facebook.com/salonmargo2k17"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white flex  items-center"
+              >
+                <MdFacebook className="mr-2" /> Facebook
+              </a>
+            </div>
+            <div className="col-lg-6 ">
+              <a
+                href="https://www.instagram.com/explore/locations/153012735297940/united-states/southington-connecticut/salon-margo/?hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white  flex  items-center"
+              >
+                <FaInstagram className="mr-2" /> Instagram
+              </a>
             </div>
           </div>
 
-          <hr className="clearfix w-100 d-md-none pb-3" />
+          <div className=" pb-2 ">
+            <FooterHeader title="contact" Icon={MdMenuBook} />
 
-          <div className="col-md-4 mb-md-0 mb-3">
-            <h5 className="text-uppercase mb-0 pb-2">
-              {/* style="border-bottom: 1px dashed white;" */}
-              <i data-feather="users"></i> Social
-            </h5>
-            <div className="row ">
-              <div className="col-lg-6 pt-3">
-                <a
-                  href="https://www.facebook.com/salonmargo2k17"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white py-3"
-                >
-                  <i data-feather="facebook"></i> Facebook
-                </a>
-              </div>
-              <div className="col-lg-6 py-3">
-                <a
-                  href="https://www.instagram.com/explore/locations/153012735297940/united-states/southington-connecticut/salon-margo/?hl=en"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white py-3"
-                >
-                  <i data-feather="instagram"></i> Instagram
-                </a>
-              </div>
-            </div>
-            <h5 id="contact" className="text-uppercase mb-0 pt-4 pb-1">
-              {/* style="border-bottom: 1px dashed white;" */}
-              <i data-feather="book-open"></i> Contact
-            </h5>
-
-            <ul className="list-unstyled footer-social-links">
-              <li>
-                <i data-feather="phone" alt="Phone"></i>
+            <ul className="list-unstyled flex flex-col space-y-4 justify-around py-6 text-xl px-6">
+              <li className="flex">
+                <FaPhone className="mr-2" />
                 <a href="tel:860-573-9228" className="text-white">
                   (860) 573-9228
                 </a>
               </li>
-              <li>
-                <i data-feather="at-sign" alt="Email"></i>
+              <li className="flex">
+                <MdEmail className="mr-2" />
                 <a href="mailto:salonmargo2k17@gmail.com" className="text-white">
                   salonmargo2k17@gmail.com
                 </a>
               </li>
             </ul>
           </div>
+        </div>
 
-          <hr className="clearfix w-100 d-md-none pb-3" />
+        <div className="mb-3">
+          <FooterHeader title="open hours" Icon={FaRegClock} />
 
-          <div className="col-md-4 mb-md-0 mb-3">
-            <h5 className="text-uppercase pb-2">
-              {/* style="border-bottom: 1px dashed white;" */}
-              <i data-feather="clock"></i> Open hours
-            </h5>
-
-            <ul className="list-unstyled">
-              <li>
-                <strong>Mon - Closed / Appt Only</strong>
-              </li>
-              <li>Tue - 10 am - 8 pm</li>
-              <li>Wed - 10 am - 6 pm</li>
-              <li>Thu - 10 am - 8 pm</li>
-              <li>Fri - 9:30 am - 6 pm</li>
-              <li>Sat - 9 am - 4pm</li>
-              <li>
-                <strong>Sun - Closed / Appt Only</strong>
-              </li>
-              <li className="mt-2">
-                Hours are <strong>flexible</strong>, so just ask!
-              </li>
-            </ul>
-          </div>
+          <ul className="list-unstyled">
+            <li>
+              <strong>Mon - Closed / Appt Only</strong>
+            </li>
+            <li>Tue - 10 am - 8 pm</li>
+            <li>Wed - 10 am - 6 pm</li>
+            <li>Thu - 10 am - 8 pm</li>
+            <li>Fri - 9:30 am - 6 pm</li>
+            <li>Sat - 9 am - 4pm</li>
+            <li>
+              <strong>Sun - Closed / Appt Only</strong>
+            </li>
+            <li className="mt-2">
+              Hours are <strong>flexible</strong>, so just ask!
+            </li>
+          </ul>
         </div>
       </div>
-
       <div className="footer-copyright text-center py-5 text-muted">
-        Salon Margo © 2020 Copyright:
-        <a target="_blank" rel="noopener noreferrer" href="https://jelizarovas.com" className="text-white">
+        Salon Margo © 2020 - {new Date().getFullYear()} Copyright:
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://jelizarovas.com"
+          className="font-bold hover:underline ml-2 text-orange-400"
+        >
           jelizarovas.com
         </a>
       </div>
-      <button onClick={topFunction} id="myBtn" title="Go to top">
-        Top
-      </button>
     </footer>
   );
 };
@@ -150,3 +134,11 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+const FooterHeader = ({ Icon, title, ...props }) => {
+  return (
+    <h5 className="uppercase px-4 pb-2 flex items-center space-x-2 text-2xl border-b border-dashed ">
+      <Icon /> <span> {title} </span>
+    </h5>
+  );
+};
